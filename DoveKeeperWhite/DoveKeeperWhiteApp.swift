38c8554713e414c89e1c36c5d081c3ca461f4ec3
esplusnin <esplusnin94@yandex.ru>
2025-09-02 18:10:39 +0300

    Initial Commit

---
+++ DoveKeeperWhite/DoveKeeperWhiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoveKeeperWhiteApp.swift
+//  DoveKeeperWhite
+//
+//  Created by Евгений on 02.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct DoveKeeperWhiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
